e4c9d946c5cf4c62c8656eaf06d7ed5903328562
mue <mue@kologlobal.com>
2013-09-24 18:39:27 +0300

    work with async

--- app/libs/utility.js
+++ app/libs/utility.js
@@ -11,15 +11,51 @@ _.extend(Utility, {
     normalize: function( value ){
         value = path.normalize(value);
         return Utility.clearLastSlash(value);
+    },
 
+    normalizeArray: function( paths ){
+        var result = [];
+        _.each(paths, function(path, i){
+            result.push( Utility.normalize(path) );
+        })
+        return result;
     },
 
     clearLastSlash: function(value){
+        //return ( value.length > 1 ) ? value.replace(/\/$/, "") : value;
         return value.replace(/\/$/, "");
     },
 
     clearRoot: function(filePath){
         return filePath.replace(root,"");
+    },
+
+    clearEmptyString: function( array ){
+        var isArray =_.isArray(array);
+        var result = [];
+
+        if(!isArray) return false;
+
+        _.each(array, function(item){
+            if (item !== "") result.push(item);
+        })
+
+        return array;
+    },
+
+    addRootToPath: function(pathItem){
+
+        var isArray =_.isArray(pathItem),
+            result = [];
+
+        if(isArray){
+            _.each(pathItem, function(currentPath){
+                result.push( root + currentPath );
+            })
+            return result;
+        }else{
+            return root + pathItem;
+        }
     }
 
 })
