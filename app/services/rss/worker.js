623858868b21bdb0357ad76731d45b077912be6c
mue <mue@kologlobal.com>
2013-10-28 17:54:21 +0200

    update queue manager progress ...

--- app/services/rss/worker.js
+++ app/services/rss/worker.js
@@ -11,58 +11,105 @@ var state = {
 
 function Worker( task ){
     this.state = state.WORKER_PREPARE_TO_WORK;
-    this.start = new Date();
-
     this.task = task;
-    this.execute();
 }
 
 Worker.prototype = {
-    execute: function(){
+
+    loadFeed: function(){
         var _this = this;
         this.state = state.WORKER_IN_WORKING_PROCESS;
+        this.start = new Date();
 
-        if(!this.valid()) return false;
+        if(!this._valid()) {
+            this.error = "task is not valid";
+            _this.calculateExecuteTime();
+            return false;
+        }
 
         async.waterfall([
             function(cb){
                 //get feed
-                Feed.findOne({_id: _this.task.data._id}, function(err, feed){
-                    if( err ) {
-                        cb(err);
-                        return false;
-                    }
-
-                    cb(null, feed);
-                });
+                _this._findFeed(cb);
             },
             function(feed, cb){
                 _this.feed = feed;
-
                 //get new post
-                _this.feed.updateFeed(function(err){
-                    if( err ) {
-                        cb(err);
-                        return false;
-                    }
-                    cb(null);
-                });
-
+                _this.feed.loadFeed(cb);
             }
         ], function(err){
-            _this.executeTime = new Date() - _this.start;
+            _this.calculateExecuteTime();
 
             if( err ){
+                _this.error = err;
                 _this.state = state.WORKER_ERROR;
                 logger.log('error', { error: err });
                 return false;
             }
             _this.state = state.WORKER_SUCCESS_DONE;
         });
+
     },
 
-    valid: function(){
+    updateFeed: function(){
+        var _this = this;
+        this.state = state.WORKER_IN_WORKING_PROCESS;
+        this.start = new Date();
+
+        if(!this._valid()) {
+            this.error = "task is not valid";
+            _this.calculateExecuteTime();
+            return false;
+        }
+
+        async.waterfall([
+            function(cb){
+                //get feed
+                _this._findFeed(cb);
+            },
+            function(feed, cb){
+                _this.feed = feed;
+                //get new post
+                _this.feed.updateFeed(cb);
+            }
+        ], function(err){
+            _this.calculateExecuteTime();
+
+            if( err ){
+                _this.error = err;
+                _this.state = state.WORKER_ERROR;
+                logger.log('error', { error: err });
+                return false;
+            }
+            _this.state = state.WORKER_SUCCESS_DONE;
+        });
+
+    },
+
+    calculateExecuteTime: function(){
+        this.end = new Date();
+        this.executeTime = this.end - this.start;
+    },
+
+    _findFeed: function(cb){
+        var _this = this;
+
+        Feed.findOne({_id: _this.task.data._id}, function(err, feed){
+            if( err ) {
+                cb(err);
+                return false;
+            }
+
+            cb(null, feed);
+        });
+    },
+
+    _valid: function(){
         return (this.task) ? true : false;
+    },
+
+    getNewPosts: function(){
+        return this.feed.newPosts;
     },
 
     getPostsRow: function(){
