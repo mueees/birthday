3de51a33397704cf54df0f3c8d1d69eac6030cdf
mue <mue@kologlobal.com>
2013-10-28 19:33:07 +0200

    start worj with rss front

---
+++ app/public/js/app/modules/rss/modules/menu/views/AddView.js
@@ -0,0 +1,26 @@
+define([
+    'marionette',
+    'text!../templates/AddView.html'
+], function(Marionette, template){
+
+    return Marionette.ItemView.extend({
+        template: _.template(template),
+
+        events: {
+
+        },
+
+        ui: {
+
+        },
+
+        initialize: function(){
+
+        },
+
+        onRender: function(){
+
+        }
+    })
+
+})
