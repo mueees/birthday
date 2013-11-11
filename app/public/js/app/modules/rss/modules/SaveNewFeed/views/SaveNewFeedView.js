fa26a194ba6e83dd2f9a41b7102d530cfef69bc1
mue <mue.miv@gmail.com>
2013-11-11 11:02:57 +0200

    work with rss

--- app/public/js/app/modules/rss/modules/SaveNewFeed/views/SaveNewFeedView.js
+++ app/public/js/app/modules/rss/modules/SaveNewFeed/views/SaveNewFeedView.js
@@ -1,8 +1,10 @@
 define([
     'marionette',
     'text!../templates/SaveNewFeedView.html',
-    'text!../templates/newCategoryTemp.html'
-], function(Marionette, template, newCategoryTemp){
+    'text!../templates/newCategoryTemp.html',
+
+    'async'
+], function(Marionette, template, newCategoryTemp, async){
 
     return Marionette.ItemView.extend({
 
@@ -12,13 +14,15 @@ define([
 
         events: {
             'click .cancel': "cancelBtn",
-            'blur #new_category': "blueNewCategory"
+            'blur #new_category': "blueNewCategory",
+            'blur .feed_name': "blurFeedName",
+            "click .create": "createBtn"
         },
 
         className: "addNewFeed",
 
         ui: {
-
+            'feed_name': '.feed_name'
         },
 
         initialize: function(options){
@@ -26,6 +30,7 @@ define([
             this.feed = options.feed;
 
             this.listenTo(this.categories, "add", this.addNewCategory);
+            this.listenTo(this.feed, "change:name", this.changeFeedName);
 
             this.render();
         },
@@ -56,6 +61,24 @@ define([
 
         },
 
+        blurFeedName: function(e){
+            if(e) e.preventDefault();
+            var $el = $(e.target),
+                value = $.trim($el.val());
+
+            if( !value ) {
+                value =  "Default Name"
+            }
+
+            this.feed.set({
+                name: value
+            })
+        },
+
+        changeFeedName: function(){
+            this.$el.find('.feed_name').val( this.feed.get('name') );
+        },
+
         addNewCategory: function(model){
             var _this = this;
 
@@ -65,9 +88,49 @@ define([
                 _this.$el.find('#new_category').val("");
 
             }).fail(function(){
-                _this.trigger("errorText", "Cannot save category");
-                debugger
+                    _this.trigger("text", "Cannot save category");
+                })
+        },
+
+        createBtn: function(e){
+            if(e) e.preventDefault();
+            var _this = this;
+
+            async.waterfall([
+                function(cb){
+                    _this.feed.save().done(function(){
+                        cb(null);
+                    }).fail(function(xhr){
+                            cb(xhr);
+                        })
+                },
+                function(cb){
+
+
+                    _this.categories.each(function(category){
+                        debugger
+                        var feedsCollection = category.get('feeds');
+                        feedsCollection.add({
+                            _id: _this.feed.get('_id')
+                        })
+                    })
+                    debugger
+
+                    _this.categories.save()
+                        .done(function(){debugger})
+                        .fail(function(){debugger})
+                }
+            ],function(err){
+                if( err ){
+                    alert(err);
+                    return false;
+                }
+                _this.trigger("text", "Cannot save feed");
             })
+        },
+
+        saveFeed: function(){
+
         },
 
         cancelBtn: function(e){
