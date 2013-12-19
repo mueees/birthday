fb3d85849d710e0497d9f880ee60275a23ee5bed
mue <mue.miv@gmail.com>
2013-12-19 07:28:26 +0200

    change cron time and rss  update time

--- app/cron.js
+++ app/cron.js
@@ -4,7 +4,7 @@ var BackupDb = require('action/cron/backupDb').BackupDb;
 var backupDb = new BackupDb();
 backupDb.execute();
 
-new cronJob('00 30 * * * *', function(){
+new cronJob('00 00 11,19 * * *', function(){
     var backupDb = new BackupDb();
     backupDb.execute();
 }, null, true, "Europe/Kiev");
